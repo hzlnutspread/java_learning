419124962824cfa8c2305dd93153c547ae0c1eaa
hzlnutspread <smithkg293@gmail.com>
2022-07-01 21:29:27 +1200

    Add threading and annotations

---
+++ src/Annotations/Cat.java
@@ -0,0 +1,23 @@
+package Annotations;
+
+@VeryImportant
+public class Cat {
+
+    @ImportantString
+    String name;
+    int age;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @RunImmediately(times = 3)
+    public void meow() {
+        System.out.println("Meow!");
+    }
+
+    public void eat() {
+        System.out.println("MONCH!");
+    }
+
+}
---
+++ src/MultiThreadingClass.java
@@ -0,0 +1,17 @@
+public class MultiThreadingClass extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Thread 1: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //System.out.println(1/0); //even if we have an error in this thread the other thread will keep going
+        }
+        System.out.println("Thread 1 has finished counting down!");
+    }
+}
