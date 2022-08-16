9d4ea3f064a7e749b1a6620f6ff6a15d411e6104
hzlnutspread <smithkg293@gmail.com>
2022-08-16 20:16:39 +1200

    Add POJO and java beans

---
+++ src/POJOandJavaBeans/POJO.java
@@ -0,0 +1,18 @@
+package POJOandJavaBeans;
+
+public class POJO {
+        public static void main(String[] args) {
+
+//        POJO = plain old java object
+//                1. Can't extend any other class
+//                2. Can't implement anything
+//                3. No outside annotations
+
+//        Java Bean
+//                1. No-args constructor
+//                2. Properties must be private
+//                3. Public getters and setters
+//                4. Must be serialisable = Implement Seriablizable interface. Can be written to databases and files.
+
+        }
+}
